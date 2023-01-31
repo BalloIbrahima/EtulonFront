3d4b0692fc250feec21aea6fd073892a2983ed6e
BalloIbrahima <ibrahimaballo01@gmail.com>
2023-01-31 12:29:53 +0000

    add game header to game page

--- src/app/dashboard/jeu/jeu.component.ts
+++ src/app/dashboard/jeu/jeu.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AdminService } from 'src/app/Service/admin/admin.service';
+import { CitoyenService } from 'src/app/Service/citoyen/citoyen.service';
+import { JeuService } from 'src/app/Service/jeu/jeu.service';
 
 @Component({
   selector: 'app-jeu',
   templateUrl: './jeu.component.html',
   styleUrls: ['./jeu.component.scss']
 })
-export class JeuComponent {
+export class JeuComponent implements OnInit{
 
+
+  nbreJeu:number=0
+  constructor(private adminService:AdminService, private citoyenService:CitoyenService, private jeuService:JeuService){}
+
+
+  ngOnInit(): void {
+
+    //recuperation du nombre de jeu
+    this.jeuService.nombrejeu().subscribe(res=>{
+      this.nbreJeu=res.data
+    })
+  }
 }
