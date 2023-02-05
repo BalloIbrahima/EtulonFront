32f7750d77a528f83d1a0f3a769e7b48900838f7
BalloIbrahima <ibrahimaballo01@gmail.com>
2023-02-05 00:18:08 +0000

    update 0028

--- src/app/dashboard/conseil/conseil.component.ts
+++ src/app/dashboard/conseil/conseil.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-conseil',
   templateUrl: './conseil.component.html',
   styleUrls: ['./conseil.component.scss']
 })
-export class ConseilComponent {
+export class ConseilComponent implements OnInit {
 
+  recherche:any=''
+  nbreConseil:any=0
+  nombrenonValide:any=0
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 }
